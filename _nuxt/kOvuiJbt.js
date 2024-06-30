b17d6649f9adc1c52a50a5455ea0a02f3394b540
kojiikeg <mouhanpatumakura@gmail.com>
2024-06-30 10:40:54 +0900

    update

---
+++ _nuxt/kOvuiJbt.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./_ogvXgc6.js";const o={};function t(r,_){return n(),c("div",null,"ここでお知らせを一覧表示する。")}const s=e(o,[["render",t],["__scopeId","data-v-7047f230"]]);export{s as default};
